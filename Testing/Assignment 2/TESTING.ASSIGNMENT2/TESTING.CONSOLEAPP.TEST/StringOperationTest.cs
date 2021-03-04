519cdc2c0d7e0e6cae819fb8fc5cd73ff95ff86b
saloniiJain <salonij828@gmail.com>
2021-03-04 21:48:46 +0530

    Fixed Commit for Testing Assignment

---
+++ Testing/Assignment 2/TESTING.ASSIGNMENT2/TESTING.CONSOLEAPP.TEST/StringOperationTest.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TESTING.CONSOLEAPP.ExtenstionClasses;
+using Xunit;
+
+namespace TESTING.CONSOLEAPP.TEST
+{
+    public class StringOperationTest
+    {
+        [Fact]
+        public void Test_ChangeCase()
+        {
+            // Arrange
+            var inputString = "Upper";
+            var outputString = "upper";
+            // Act
+            var newString = inputString.ChangeCase();
+            // Assert
+            Assert.Equal(newString, outputString);
+        }
+        [Fact]
+        public void Test_ChangeToTitleCase()
+        {
+            // Arrange
+            var inputString = "ram MCA RAM Bca BbA";
+            var outputString = "Ram MCA RAM Bca Bba";
+            // Act
+            var newString = inputString.ChangeToTitleCase();
+            // Assert
+            Assert.Equal(newString, outputString);
+        }
+        [Fact]
+        public void Test_IsLowerCaseString()
+        {
+            // Arrange
+            var inputString = "ramsingh";
+            // Act
+            var newString = inputString.IsLowerCaseString();
+            // Assert
+            Assert.True(newString);
+        }
+        [Fact]
+        public void Test_IsUpperCaseString()
+        {
+            // Arrange
+            var inputString = "RAM";
+            // Act
+            var newString = inputString.IsUpperCaseString();
+            // Assert
+            Assert.True(newString);
+        }
+        [Fact]
+        public void Test_DoCapitalize()
+        {
+            // Arrange
+            var inputString = "ram";
+            var outputString = "Ram";
+            // Act
+            var newString = inputString.DoCapitalize();
+            // Assert
+            Assert.Equal(newString,outputString);
+        }
+        [Theory]
+        [InlineData("123", true)]
+        [InlineData("123c", false)]
+        public void Test_IsValidNumericValue(string inputString,bool result)
+        {
+            // Arrange
+            
+            // Act
+            var newString = inputString.IsValidNumericValue();
+            // Assert
+            Assert.Equal(newString, result);
+        }
+        [Fact]
+        public void Test_RemoveLastCharacter()
+        {
+            // Arrange
+            var inputString = "Ram Singh";
+            var outputString = "Ram Sing";
+            // Act
+            var newString = inputString.RemoveLastCharacter();
+            // Assert
+            Assert.Equal(newString, outputString);
+        }
+        [Fact]
+        public void Test_WordCount()
+        {
+            // Arrange
+            var inputString = "Ram Singh";
+            var count = 2;
+            // Act
+            var newString = inputString.WordCount();
+            // Assert
+            Assert.Equal(newString, count);
+        }
+        [Fact]
+        public void Test_StringToInteger()
+        {
+            // Arrange
+            var inputString = "123";
+            var output = 123;
+            // Act
+            var newString = inputString.StringToInteger();
+            // Assert
+            Assert.Equal(newString, output);
+        }
+    }
+}
---
+++ Testing/Assignment 2/TESTING.ASSIGNMENT2/TESTING.CONSOLEAPP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TESTING.CONSOLEAPP.ExtenstionClasses;
+
+namespace TESTING.CONSOLEAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ram".ChangeCase());
+            Console.WriteLine("ram MCA RAM Bca BbA".ChangeToTitleCase());
+            Console.WriteLine("Ram".IsLowerCaseString()?"All Characters are lower":"Not all characters are lower");
+            Console.WriteLine("ramsingh".DoCapitalize());
+            Console.WriteLine("ram".IsUpperCaseString() ? "All Characters are upper" : "Not all characters are upper");
+            Console.WriteLine("123d".IsValidNumericValue()?"Is valid numeric string":"Is not valid numeric string");
+            Console.WriteLine("ramsingh".RemoveLastCharacter());
+            Console.WriteLine("ram singh".WordCount());
+            Console.WriteLine("123".StringToInteger());
+            Console.ReadLine();
+        }
+    }
+}
